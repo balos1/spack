9ee388437b58ad20bf21ddf99606a4b6e11be0ca
Satish Balay <balay@mcs.anl.gov>
2022-10-10 13:22:49 -0500

    hiop@1.4.1

--- var/spack/repos/builtin/packages/xsdk/package.py
+++ var/spack/repos/builtin/packages/xsdk/package.py
@@ -498,4 +498,4 @@ class Xsdk(BundlePackage, CudaPackage, ROCmPackage):
     xsdk_depends_on("exago@1.4.1", when="@0.8.0 +exago")
 
     xsdk_depends_on("hiop@develop", when="@develop +hiop")
-    xsdk_depends_on("hiop@0.6.2", when="@0.8.0 +hiop")
+    xsdk_depends_on("hiop@0.7.0", when="@0.8.0 +hiop")
