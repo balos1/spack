2e940170e0fb1db418f6489a41f30028f2fc2330
Pieter Ghysels <pghysels@lbl.gov>
2022-10-15 00:07:36 -0500

    STRUMPACK enable cuda, openmp and slate variants

--- var/spack/repos/builtin/packages/xsdk/package.py
+++ var/spack/repos/builtin/packages/xsdk/package.py
@@ -372,8 +372,8 @@ class Xsdk(BundlePackage, CudaPackage, ROCmPackage):
     xsdk_depends_on("omega-h@9.19.1", when="@0.4.0 +omega-h")
 
     xsdk_depends_on("strumpack ~cuda", when="~cuda @0.6.0: +strumpack")
-    xsdk_depends_on("strumpack@master~slate~openmp", when="@develop +strumpack")
-    xsdk_depends_on("strumpack@7.0.1~slate~openmp", when="@0.8.0 +strumpack")
+    xsdk_depends_on("strumpack@master", when="@develop +strumpack", cuda_var=["cuda"])
+    xsdk_depends_on("strumpack@7.0.1", when="@0.8.0 +strumpack", cuda_var=["cuda"])
     xsdk_depends_on("strumpack@6.1.0~slate~openmp", when="@0.7.0 +strumpack")
     xsdk_depends_on("strumpack@5.0.0~slate~openmp", when="@0.6.0 +strumpack")
     xsdk_depends_on("strumpack@3.3.0~slate~openmp", when="@0.5.0 +strumpack")
