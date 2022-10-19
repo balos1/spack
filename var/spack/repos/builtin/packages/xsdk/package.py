8107ef9cf3ee26d1df9e65faf0064f1bee3d7d25
Satish Balay <balay@mcs.anl.gov>
2022-10-18 23:16:38 -0500

    fix strumpack

--- var/spack/repos/builtin/packages/xsdk/package.py
+++ var/spack/repos/builtin/packages/xsdk/package.py
@@ -373,7 +373,7 @@ class Xsdk(BundlePackage, CudaPackage, ROCmPackage):
     xsdk_depends_on("omega-h@9.19.1", when="@0.4.0 +omega-h")
 
     xsdk_depends_on("strumpack ~cuda", when="~cuda @0.6.0: +strumpack")
-    xsdk_depends_on("~strumpack ~openmp", when="~slate @0.8.0: +strumpack")
+    xsdk_depends_on("strumpack ~slate~openmp", when="~slate @0.8.0: +strumpack")
     xsdk_depends_on("strumpack@master", when="@develop +strumpack", cuda_var=["cuda"])
     xsdk_depends_on("strumpack@7.0.1", when="@0.8.0 +strumpack", cuda_var=["cuda"])
     xsdk_depends_on("strumpack@6.1.0~slate~openmp", when="@0.7.0 +strumpack")
