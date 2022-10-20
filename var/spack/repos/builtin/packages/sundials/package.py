a1f15f4dba8ceb5b6206d52197472b5f207ad484
Balos, Cody Joe <balos1@llnl.gov>
2022-10-20 13:29:52 -0700

    sundials: link to kokkos if petsc+kokkos

--- var/spack/repos/builtin/packages/sundials/package.py
+++ var/spack/repos/builtin/packages/sundials/package.py
@@ -27,7 +27,7 @@ class Sundials(CMakePackage, CudaPackage, ROCmPackage):
     # Versions
     # ==========================================================================
     version("develop", branch="develop")
-    version("6.4.0", branch="develop")
+    version("6.4.0", branch="release/6.4.0")
     version("6.3.0", sha256="89a22bea820ff250aa7239f634ab07fa34efe1d2dcfde29cc8d3af11455ba2a7")
     version("6.2.0", sha256="195d5593772fc483f63f08794d79e4bab30c2ec58e6ce4b0fb6bcc0e0c48f31d")
     version("6.1.1", sha256="cfaf637b792c330396a25ef787eb59d58726c35918ebbc08e33466e45d50470c")
@@ -428,6 +428,9 @@ class Sundials(CMakePackage, CudaPackage, ROCmPackage):
         if "+petsc" in spec:
             if spec.version >= Version("5"):
                 args.append(define("PETSC_DIR", spec["petsc"].prefix))
+                if "+kokkos" in spec["petsc"]:
+                    args.append(define("Kokkos_DIR", spec["kokkos"].prefix))
+                    args.append(define("KokkosKernels_DIR", spec["kokkos-kernels"].prefix))
             else:
                 args.extend(
                     [
