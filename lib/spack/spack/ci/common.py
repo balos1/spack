b4e7e189fb907e93d72c289dcbf869c7558aa4ac
Cody J. Balos <balos1@llnl.gov>
2026-05-29 10:33:31 -0700

    print backtrace in spack ci rebuild

--- lib/spack/spack/ci/common.py
+++ lib/spack/spack/ci/common.py
@@ -583,7 +583,7 @@ class SpackCIConfig:
                         "cd {env_dir}",
                         "spack env activate --without-view .",
                         "spack spec /$SPACK_JOB_SPEC_DAG_HASH",
-                        "spack ci rebuild",
+                        "spack -t -d ci rebuild",
                     ]
                 }
             },
