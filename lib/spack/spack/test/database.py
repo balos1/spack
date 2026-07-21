182f509c8cdf9c7d3156ecae50163ae1986765a0
Balos, Cody, J <balos1@llnl.gov>
2026-07-20 21:39:18 -0700

    merge fix

--- lib/spack/spack/test/database.py
+++ lib/spack/spack/test/database.py
@@ -38,6 +38,8 @@ import spack.paths
 import spack.repo
 import spack.spec
 import spack.store
+import spack.util.filesystem as fs
+import spack.util.lock as lk
 import spack.version as vn
 from spack.enums import InstallRecordStatus
 from spack.installer import PackageInstaller
